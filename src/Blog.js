5f4c049363c6fb3bef72b7f3f1a438368700b318
Afzal Ali <72311983+AfzalAli231@users.noreply.github.com>
2022-02-19 10:53:34 +0500

    Update Blog.js

--- src/Blog.js
+++ src/Blog.js
@@ -34,21 +34,6 @@ function Blog() {
                  </div>
               </div>
 
-              <div className="col__3">
-              <div className="project__box pointer relative">
-                     <div className="project__box__img pointer relative">
-                         <div className="project__img__box">
-                             <img src={Blog2} alt="" className="project__img" />
-                         </div>
-                         <div className="mask__effect"></div>
-                     </div>
-                      <div className="Blog__meta absolute">
-                         <h5 className="project__text">Development</h5>
-                         <h4 className="project__text">Getting tickets to the big show</h4>
-                         <button className="blog project__btn btn">Read More</button>
-                       </div>
-                 </div>
-              </div>
 
               <div className="col__3">
               <div className="project__box pointer relative">
